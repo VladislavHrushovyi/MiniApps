dd88444cf7cee34bf707ed329145dd63a9be9a93
vladg <vlad.grush.99@gmail.com>
2024-10-10 19:19:29 +0300

    some basic example

--- LamdaPractice/Program.cs
+++ LamdaPractice/Program.cs
@@ -1,3 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿
+using System.Runtime.InteropServices;using System.Text.Json.Serialization.Metadata;
 
-Console.WriteLine("Hello, World!");
+var plus = (int a, int b) => a + b;
+
+static int Wrap(Func<int, int, int> func)
+{
+    Console.WriteLine("Start wrap");
+    var fetchedData = Task.Run<Tuple<int, int>>( async () =>
+        {
+            await Task.Delay(1000);
+            return new Tuple<int, int>(2,2);
+        }).GetAwaiter().GetResult();
+    var result = func.Invoke(fetchedData.Item1, fetchedData.Item2);
+
+    return result;
+}
+
+Console.WriteLine(Wrap(plus));
