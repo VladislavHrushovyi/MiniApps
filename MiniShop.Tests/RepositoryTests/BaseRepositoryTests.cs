101a9dbaa512e21741d46ed93a091f7a08a12c18
vladg <vlad.grush.99@gmail.com>
2024-11-02 19:37:50 +0200

    first test

---
+++ MiniShop.Tests/RepositoryTests/BaseRepositoryTests.cs
@@ -0,0 +1,29 @@
+﻿using MiniShop.Application.Repository;
+using MiniShop.Domain.Models;
+using Moq;
+using Xunit;
+
+namespace MiniShop.Tests.RepositoryTests;
+
+public class BaseRepositoryTests
+{
+    [Fact]
+    public void AddItem_Test()
+    {
+        var user = new User()
+        {
+            Id = Guid.NewGuid(),
+            Checks = new List<Check>(),
+            Date = DateTime.Now,
+            Nickname = "test",
+            RoleUser = new UserRole(),
+            RoleUserId = Guid.NewGuid()
+        };
+        var baseRepositoryMock = new Mock<IBaseRepository<User>>();
+        baseRepositoryMock.Setup(x => x.Add(user)).ReturnsAsync(user);
+        
+        var result  = baseRepositoryMock.Object.Add(user).GetAwaiter().GetResult();
+        
+        Assert.Equal(user.Id, result.Id);
+    }
+}
--- MiniShop.Tests/UnitTest1.cs
+++
@@ -1,9 +0,0 @@
-namespace MiniShop.Tests;
-
-public class UnitTest1
-{
-    [Fact]
-    public void Test1()
-    {
-    }
-}
